b3ddc4e427d890879627ae3283433bb9c7e5c9d6
Saswati <saswati.das@ife.no>
2022-11-07 14:25:50 +0100

    minor refactor

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
                         <p className='text-pink-600 text-lg'>My name is</p>
                         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Saswati Das</h1>
                         <h2 className='text-3xl sm:text-5xl font-bold text-[#8892b0]'>
-                            I'm a Full Stack Developer.
+                            I'm a Front End Developer.
                         </h2>
                         <p className='text-[#8892b0] py-4 max-w-[700px]'>
                             I’m a web developer developer specializing in building (and occasionally
